5e4e1ee697ce7eec7874bd627311cc4d3f81a667
Kevin Littles <pro.kevinribeiro@gmail.com>
2024-01-17 10:07:47 -0300

    implement inversion algorithm and tests

---
+++ Stanford Alogrithm Specialization/Tests/DivideAndConquer/Inversion.swift
@@ -0,0 +1,65 @@
+//
+//  InversionCount.swift
+//  Tests
+//
+//  Created by Kevin Littles on 16/01/24.
+//
+
+import Foundation
+
+struct Inversion {
+    
+    static var cumulativeInversionsCount: Int = 0
+    
+    static func count(_ array: [Int]) -> Int {
+        mergeSort(array)
+        return cumulativeInversionsCount
+    }
+    
+    @discardableResult
+    private static func mergeSort(_ array: [Int]) -> [Int] {
+        guard array.count > 1 else {
+            return array
+        }
+        
+        var (left, right): ([Int], [Int]) = divide(array)
+        left = mergeSort(left)
+        right = mergeSort(right)
+        
+        return conquer(left, right: right)
+    }
+    
+    private static func divide(_ array: [Int]) -> ([Int], [Int]) {
+        let midIndex: Int = array.count/2
+        return (Array(array[..<midIndex]), Array(array[midIndex...]))
+    }
+    
+    private static func conquer(_ left: [Int], right: [Int]) -> [Int] {
+        
+        let conqueredArrayCount: Int = left.count + right.count
+        var conqueredArray: [Int] = []
+        
+        var leftIndex: Int = 0
+        var rightIndex: Int = 0
+        
+        for _ in 0..<conqueredArrayCount {
+            if leftIndex >= left.count {
+                conqueredArray.append(contentsOf: Array(right[rightIndex...]))
+                break
+            } else if rightIndex >= right.count {
+                conqueredArray.append(contentsOf: Array(left[leftIndex...]))
+                break
+            } else if left[leftIndex] <= right[rightIndex] {
+                conqueredArray.append(left[leftIndex])
+                leftIndex += 1
+            } else {
+                conqueredArray.append(right[rightIndex])
+                rightIndex += 1
+                cumulativeInversionsCount += left.count - leftIndex
+            }
+        }
+        
+        return conqueredArray
+    }
+    
+}
